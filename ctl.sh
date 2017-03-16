c46e0526815b19df2c5cba1cabb0c37ab76e5227
Adam Scislowicz <adam@vulcanize.io>
2017-03-16 16:55:23 -0700

    add start/stop and status commands to ctl.sh

--- ctl.sh
+++ ctl.sh
@@ -2,8 +2,6 @@
 
 cmd=$1
 
-echo $cmd
-
 function which_cmd {
 	which $1 &> /dev/null
 	if test $? -ne 0; then 
@@ -24,19 +22,68 @@ which_cmd kops
 echo -e "kops Version: \c"
 kops version
 
+if test "$cmd" = "start"; then
+	kops create cluster --cloud=aws --zones=us-west-1c --name=vulcanize.io --state s3://test-kops-ads-state-store/
+	kops update cluster vulcanize.io --yes --state s3://test-kops-ads-state-store/
+	let elapsed=0
+	let timeout=600
+	echo "Waiting for up to 10min for cluster to spin up..."
+	kubectl get nodes &> /dev/null
+	while test $? -ne 0; do
+		if test $elapsed -gt $timeout; then
+			echo "ERROR: Timeout ($elapsed > $timeout) seconds"
+			echo "Shutting down..."
+			./ctl.sh stop
+			exit 1
+		fi
+		sleep 1
+		let elapsed=elapsed+1
+		kubectl get nodes &> /dev/null
+	done
+	kubectl run my-nginx --image=nginx --replicas=1 --port=80
+	kubectl expose deployment my-nginx --port=80 --type=LoadBalancer 
+elif test "$cmd" = "stop"; then
+	kops delete cluster --state s3://test-kops-ads-state-store/ --name vulcanize.io --yes
+elif test "$cmd" = "status"; then
+	echo "---=== Clusters:"
+	kops get cluster --state s3://test-kops-ads-state-store/ &> tmp.log
+	if test "No clusters found" = "$(cat tmp.log)"; then
+		cat tmp.log
+	else
+		cat tmp.log
+		echo "---=== Nodes:"
+		kubectl get nodes -o wide
+		echo "---=== Deployments:"
+		kubectl get deployments -o wide
+		echo "---=== Services:"
+		kubectl get services -o wide
+	fi
+	rm -f tmp.log
+fi
+
 # aws route53 list_hosted_zones
 # aws s3 ls
 
 # kops get cluster
 
-# kops create cluster --cloud=aws --zones=us-west-1c --name=uswest1.vulcanize.io
-# kops edit cluster uswest1.vulcanize.io --yes
-# kops update cluster uswest1.vulcanize.io --yes
+# kops create cluster --cloud=aws --zones=us-west-1c --name=vulcanize.io --state s3://test-kops-ads-state-store/
+# kops edit cluster vulcanize.io
+# kops update cluster vulcanize.io --yes --state s3://test-kops-ads-state-store/
 # ...
 # kubectl get nodes
 # kubectl create -f https://rawgit.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml
 # kubectl proxy --port=8080 &
 # ...
-# kops delete cluster --name=uswest1.vulcanize.io --yes
+# kubectl run my_nginx --image=nginx --replicas=1 --port=80
+# kubectl get pods
+# kubectl get deployments
+# kubectl expose pod podname --port=80 --name=outname
+# kubectl expose deployment my-nginx --port=80 --type=LoadBalancer
+# kubectl get services -o wide
+# kubectl -o wide --all-namespaces=true get ep
+# ---
+# kubectl run my-ethermint --image=tendermint/ethermint --replicas=2 --port=22
+# ...
+# kops delete cluster --state s3://test-kops-ads-state-store/ --name vulcanize.io
 
 echo "exiting normally."
